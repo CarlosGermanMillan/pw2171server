0604bfd2de279106053015dc20e160ed90db04e4
CarlosGermanMillan <12170514@itculiacan.edu.mx>
2017-05-24 15:04:27 -0600

    html css JSON

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -2,22 +2,22 @@
 var iniciaApp = function(){
 	//alert("Hola App");
 	var entrar = function(){
-		alert($("#txtUsuario").val());
-		alert($("#txtClave").val());
+		alert($("txtUsuario").val());
+		alert($("txtClave").val());
 	}
 	var teclaUsuario = function(tecla){
-		if(tecla.which==13){
-			$("#txtClave").focus();
+		if(tecla.wich==13){
+			$("txtClave").focus();
 		}
 	}
-	var teclaClave = function(tecla){
-		if(tecla.which==13){
+	var teclaClave= function(tecla){
+		if(tecla.wich==13){
 			entrar();
 		}
 	}
 	//Sección de declaración de eventoss
 	$("#btnEntrar").on("click",entrar);
-	$("#txtUsuario").on("keypress",teclaUsuario);
-	$("#txtClave").on("keypress",teclaClave);
+	$("txtUsuario").on("keypress",teclaUsuario);
+	$("txtClave").on("keypress",teclaClave);
 }
 $(document).ready(iniciaApp);
